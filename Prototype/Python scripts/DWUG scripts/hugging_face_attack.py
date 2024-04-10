fdead2056038a35382938c03838f04f6cecfb640
Marcel W <st163937@stud.uni-stuttgart.de>
2024-04-10 10:30:33 +0200

    CLean up and add attacker

--- Prototype/Python scripts/DWUG scripts/hugging_face_attack.py
+++ Prototype/Python scripts/DWUG scripts/hugging_face_attack.py
@@ -1,19 +1,130 @@
 import os
 import csv
 from pathlib import Path
+from WordTransformer import WordTransformer, InputExample
+from sentence_transformers import util
 
-use_pairs_input_path = 'data_output/dwug_en/data/afternoon_nn/pairs_clean.csv'
-output_path = 'data_output/attack_pairs'
-Path(output_path).mkdir(parents=True, exist_ok=True)
+model = WordTransformer('pierluigic/xl-lexeme')
+
+print('model loaded')
+use_pairs_input_path = 'data_output/dwug_en/data/attack_nn/pairs.csv'
+output_path_pairs = 'data_output/attack_pairs'
+Path(output_path_pairs).mkdir(parents=True, exist_ok=True)
+
+output_path_list_challenge = 'data_output/attack_list_challenge'
+Path(output_path_pairs).mkdir(parents=True, exist_ok=True)
 
 
-def load_pairs(file_path):
-    pairs = []
+def load_model():
+    print('loading model...')
+    model
+
+def load_csv_file(file_path):
+    data = []
     with open(file_path, encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, strict=True)
         for row in reader:
-            pairs.append(row)
-    return pairs
+            data.append(row)
+    return data
 
 
-pairs = load_pairs(use_pairs_input_path)
+def analyse_pair(pair):
+    # Get the context
+    lemma = pair['lemma']
+    context1 = pair['context1']
+    context2 = pair['context2']
+
+    #print('lemma:', lemma)
+
+    #print('context:', context1)
+
+    start_index1 = context1.find(lemma)
+    start_index2 = context2.find(lemma)
+
+    # Get the target word and its position
+    target_position1 = [int(pair['indexes_target_token1'].split(':')[0]),
+                        int(pair['indexes_target_token1'].split(':')[1])]
+    derived_indexes1_given = pair['derived_indexes1']
+    derived_indexes1 = [start_index1, start_index1 + len(lemma)]
+    derived_indexes2 = [start_index2, start_index2 + len(lemma)]
+
+    print('target_position:', target_position1)
+    print('derived_position_given:', derived_indexes1_given)
+    print('derived_position:', derived_indexes1)
+
+    print('derived word1: ', context1[derived_indexes1[0]:derived_indexes1[1]])
+    print('target context1: ',context1)
+    print('target word1: ', context1[target_position1[0]: target_position1[1]])
+
+    # Create an InputExample
+    input_example1 = InputExample(texts=context1, positions=derived_indexes1)
+    input_example2 = InputExample(texts=context2, positions=derived_indexes2)
+
+    print('example:', input_example1)
+
+    # Get the embedding of the target word
+    target_embedding1 = model.encode(input_example1, convert_to_tensor=True)
+    target_embedding2 = model.encode(input_example2, convert_to_tensor=True)
+
+    cosine_similarity = util.cos_sim(target_embedding1, target_embedding2)
+
+    return cosine_similarity
+
+
+def output_pairs(file_path, data, keys):
+    with open(file_path, 'w', newline='') as f:
+        w = csv.DictWriter(f, keys, delimiter='\t', quoting=csv.QUOTE_NONE, escapechar=' ', extrasaction='ignore',
+                           lineterminator='\n')
+        w.writeheader()
+        w.writerows(data)
+
+
+def generate_and_write_cosine_similarity(pairs):
+    print('Generating cosine similarity...')
+    keys = ['lemma', 'identifier1', 'identifier2', 'context1', 'context2', 'indexes_target_token1',
+            'derived_indexes1', 'indexes_target_token2', 'derived_indexes2', 'judgment', 'cosine_similarity']
+    data = []
+    count = 1
+    lemma = pairs[0]['lemma']
+    for pair in pairs[:10]:
+        print('for pair ', count, ' of ', len(pairs))
+        count += 1
+        cosine_similarity = analyse_pair(pair)
+
+        pair['cosine_similarity'] = cosine_similarity.item()
+        data.append(pair)
+    output_pairs(os.path.join(output_path_pairs, 'pairs_attack_' + lemma + '_.csv'), data, keys)
+    print('Cosine similarity generated. Save to:', output_path_pairs + '/pairs_attack_' + lemma + '_.csv')
+
+
+def analyze_list_challenge(list_challenge, uses):
+    lemma = list_challenge['lemma']
+    reference_usage = filter(lambda x: x['identifier'] == list_challenge['reference_usage'], uses)
+    print('reference_usage:', reference_usage)
+    return 0
+
+
+def generate_and_write_list_challenge(challenges, uses, judgments):
+    print('Analysing list challenges...')
+    keys = ['lemma', 'reference_usage', 'reference_usage_context', 'reference_usage_indexes_target_token',
+            'reference_usage_derived_indexes', 'cosine_similarity']
+    data = []
+    count = 1
+    for list_challenge in challenges:
+        print('for list_challenge ', count, ' of ', len(list_challenge))
+        count += 1
+        cosine_similarity = analyze_list_challenge(list_challenge, uses)
+        list_challenge['cosine_similarity'] = cosine_similarity.item()
+        data.append(list_challenge)
+    output_pairs(os.path.join(output_path_list_challenge, 'list_challenge_attack.csv'), data, keys)
+    print('List challenge generated. Save to:', output_path_list_challenge + '/list_challenge.csv')
+
+
+uses = load_csv_file('data_output/dwug_en/data/attack_nn/uses.csv')
+judgments = load_csv_file('data_output/dwug_en/data/attack_nn/judgments.csv')
+list_challenges = load_csv_file('data_output/dwug_en/data/attack_nn/list_challenge.csv')
+
+#analyze_list_challenge(list_challenges[0], uses)
+
+pairs = load_csv_file(use_pairs_input_path)
+generate_and_write_cosine_similarity(pairs)
--- Prototype/Python scripts/DWUG scripts/prototype_pairs.py
+++ Prototype/Python scripts/DWUG scripts/prototype_pairs.py
@@ -9,7 +9,10 @@ Path(output_path).mkdir(parents=True, exist_ok=True)
 
 undesired_judgments = ['0.0']
 undesired_annotators = []
-undesired_keys = ['identifier_system', 'project', 'lang', 'user']
+undesired_keys = ['identifier_system', 'project', 'lang', 'user', 'pos', 'date', 'grouping', 'description',
+                  'indexes_target_sentence',
+                  'context_tokenized', 'indexes_target_token_tokenized', 'indexes_target_sentence_tokenized',
+                  'context_lemmatized', 'context_pos']
 
 
 def write_csv(file_path, data, keys):
@@ -20,9 +23,17 @@ def write_csv(file_path, data, keys):
         w.writerows(data)
 
 
+def read_csv(file_path):
+    return read_and_filter_csv(file_path, [], [], [])
+
+#filter später
 def read_and_filter_csv(file_path, undesired_keys, undesired_judgments, undesired_annotators):
     with open(file_path, encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, strict=True)
+        #header = reader.__next__()
+
+        #print('row:', header.keys())
+
         rows = [row for row in reader]
         filtered_rows = []
         for row in rows:
@@ -35,11 +46,16 @@ def read_and_filter_csv(file_path, undesired_keys, undesired_judgments, undesire
                 row_out['comment'] = ' '
             if 'annotator' in row_out and row_out['annotator'] == 'AndreaMariaC':
                 row_out['annotator'] = 'amariac810'
+            if 'context' in row:
+                lemma = row['lemma'].split('_')[0]
+                row_out['lemma'] = lemma
+                row_out['derived_indexes'] = derive_indexes(lemma, row['context'])
+
             filtered_rows.append(row_out)
         return filtered_rows
 
 
-def generate_random_pairs(uses, judgments):
+def generate_pairs(uses, judgments):
     pairs = []
     uses_dict = {use['identifier']: use for use in uses}
 
@@ -49,13 +65,15 @@ def generate_random_pairs(uses, judgments):
         use2 = uses_dict.get(judgment['identifier2'])
         if use1 is not None and use2 is not None:
             pair = {
-                'lemma': use1['lemma'],  # Assuming that the lemma is the same for both uses
+                'lemma': use1['lemma'],
                 'identifier1': use1['identifier'],
                 'identifier2': use2['identifier'],
                 'context1': use1['context'],
                 'context2': use2['context'],
                 'indexes_target_token1': use1['indexes_target_token'],
+                'derived_indexes1': use1['derived_indexes'],
                 'indexes_target_token2': use2['indexes_target_token'],
+                'derived_indexes2': use2['derived_indexes'],
                 'judgment': judgment['judgment'],
             }
             pairs.append(pair)
@@ -65,6 +83,62 @@ def generate_random_pairs(uses, judgments):
     return pairs
 
 
+def generate_and_print_list_challenge():
+    for dataset in datasets:
+        dataset_path = os.path.join(input_path, dataset, 'data')
+        for p in Path(dataset_path).glob('*/'):
+            lemma_path = str(p).split('/')[-1].replace('-', '_')
+            uses_file_path = os.path.join(str(p), 'uses.csv')
+            judgments_file_path = os.path.join(str(p), 'judgments.csv')
+
+            lemma = os.path.basename(lemma_path)
+
+            uses = read_and_filter_csv(uses_file_path, undesired_keys, undesired_judgments, undesired_annotators)
+            judgments = read_and_filter_csv(judgments_file_path, [], undesired_judgments, undesired_annotators)
+            print('Generating list of challenges ...')
+
+            local_challenges = []
+
+            for usage in uses:
+                local_list_challenge = find_practical_reference_usages(usage, judgments)
+                if local_list_challenge['identifier3'] is not None:
+                    local_challenges.append(local_list_challenge)
+
+            list_challenge_header = local_challenges[0].keys() if local_challenges else []
+            output_path_lemma = os.path.join(output_path, dataset, 'data', lemma)
+            Path(output_path_lemma).mkdir(parents=True, exist_ok=True)
+
+            write_csv(os.path.join(output_path_lemma, 'list_challenges.csv'), local_challenges, list_challenge_header)
+
+
+def find_practical_reference_usages(usage, judgments):
+    judgment_to_have = ['1.0', '2.0', '3.0', '4.0']
+    list_challenge = {
+        'lemma': usage['lemma'],
+        'identifier_ref': usage['identifier'],
+        'identifier1': None,
+        'judgment1': None,
+        'identifier2': None,
+        'judgment2': None,
+        'identifier3': None,
+        'judgment3': None,
+        'identifier4': None,
+        'judgment4': None
+    }
+    i = 1
+    for judgment in judgments:
+        if judgment['identifier1'] == usage['identifier']:
+            if judgment['judgment'] in judgment_to_have:
+                list_challenge['identifier' + str(i)] = judgment['identifier2']
+                list_challenge['judgment' + str(i)] = judgment['judgment']
+                judgment_to_have.remove(judgment['judgment'])
+                i += 1
+                if len(judgment_to_have) == 0:
+                    break
+
+    return list_challenge
+
+
 def remove_judgments_from_pairs(pairs):
     for pair in pairs:
         pair.pop('judgment', None)
@@ -72,34 +146,58 @@ def remove_judgments_from_pairs(pairs):
     return pairs
 
 
-for dataset in datasets:
-    dataset_path = os.path.join(input_path, dataset, 'data')
-    for p in Path(dataset_path).glob('*/'):
-        print(p)
-        lemma_path = str(p).split('/')[-1].replace('-', '_')
-        uses_file_path = os.path.join(str(p), 'uses.csv')
-        judgments_file_path = os.path.join(str(p), 'judgments.csv')
-        pairs_file_path = os.path.join(str(p), 'pairs.csv')
-
-        lemma = os.path.basename(lemma_path)
+def derive_indexes(lemma, context):
+    start_index = context.find(lemma)
+    #print('derived_lemma: ', context[start_index:start_index + len(lemma)])
+    return [start_index, start_index + len(lemma)]
 
 
-        uses = read_and_filter_csv(uses_file_path, undesired_keys, undesired_judgments, undesired_annotators)
-        judgments = read_and_filter_csv(judgments_file_path, [], undesired_judgments, undesired_annotators)
-        pairs_header = read_and_filter_csv(pairs_file_path, [], undesired_judgments,
-                                           undesired_annotators)[0].keys() if pairs_file_path else []
+def generate_and_print_set_of_pairs():
+    for dataset in datasets:
+        dataset_path = os.path.join(input_path, dataset, 'data')
+        for p in Path(dataset_path).glob('*/'):
+            lemma_path = str(p).split('/')[-1].replace('-', '_')
+            uses_file_path = os.path.join(str(p), 'uses.csv')
+            judgments_file_path = os.path.join(str(p), 'judgments.csv')
+            pairs_file_path = os.path.join(str(p), 'pairs.csv')
 
-        pairs = generate_random_pairs(uses, judgments)
+            lemma = os.path.basename(lemma_path)
 
-        output_path_lemma = os.path.join(output_path, dataset, 'data', lemma)
-        Path(output_path_lemma).mkdir(parents=True, exist_ok=True)
+            uses = read_and_filter_csv(uses_file_path, undesired_keys, undesired_judgments, undesired_annotators)
+            judgments = read_and_filter_csv(judgments_file_path, [], undesired_judgments, undesired_annotators)
+            pairs_header = ['lemma', 'identifier1', 'identifier2', 'context1', 'context2', 'indexes_target_token1',
+                            'derived_indexes1',
+                            'indexes_target_token2', 'derived_indexes2', 'judgment']
 
-        print('output path:', output_path_lemma)
+            pairs = generate_pairs(uses, judgments)
+
+            output_path_lemma = os.path.join(output_path, dataset, 'data', lemma)
+            Path(output_path_lemma).mkdir(parents=True, exist_ok=True)
+
+            write_csv(os.path.join(output_path_lemma, 'judgments.csv'), judgments,
+                      judgments[0].keys() if judgments else [])
+            write_csv(os.path.join(output_path_lemma, 'uses.csv'), uses, uses[0].keys() if uses else [])
+            write_csv(os.path.join(output_path_lemma, 'pairs.csv'), pairs, pairs_header)
+            write_csv(os.path.join(output_path_lemma, 'pairs_clean.csv'), remove_judgments_from_pairs(pairs),
+                      pairs_header)
 
 
+def analyse_judgment_cosine_correlation(path):
+    pairs = read_csv(path)
+    data = []
+    lemma = pairs[0]['lemma']
+    file_name = lemma + '_jcc.csv'
+    output_path_local = output_path + '/judgment_cosine_correlation/' + lemma
+    Path(output_path_local).mkdir(parents=True, exist_ok=True)
+    for pair in pairs:
+        judgment = pair['judgment']
+        cosine_similarity = pair['cosine_similarity']
+        data.append({'lemma': lemma, 'judgment': judgment, 'cosine_similarity': cosine_similarity})
+
+    write_csv(os.path.join(output_path_local, file_name), data, ['judgment', 'cosine_similarity'])
 
 
-        write_csv(os.path.join(output_path_lemma, 'judgments.csv'), judgments, judgments[0].keys() if judgments else [])
-        write_csv(os.path.join(output_path_lemma, 'uses.csv'), uses, uses[0].keys() if uses else [])
-        write_csv(os.path.join(output_path_lemma, 'pairs.csv'), pairs)
-        write_csv(os.path.join(output_path_lemma, 'pairs_clean.csv'), remove_judgments_from_pairs(pairs), pairs_header)
+generate_and_print_list_challenge()
+
+#analyse_judgment_cosine_correlation('data_output/attack_pairs/pairs_attack.csv')
+#generate_pairs()
