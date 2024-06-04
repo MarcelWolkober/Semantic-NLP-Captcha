05108e34c7a8bf35058e0f387f81a7a4568d3bff
Marcel W <st163937@stud.uni-stuttgart.de>
2024-06-04 10:40:24 +0200

    config changes

--- Prototype/captcha-frontend/src/App.vue
+++ Prototype/captcha-frontend/src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>    |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </div>
-<!--  <img alt="Vue logo" src="./assets/logo.png" />-->
+  <!--  <img alt="Vue logo" src="./assets/logo.png" />-->
   <router-view />
 </template>
 
 <script>
-import PairComponent from "@/components/challenges/PairComponent.vue";
 
 export default {
   name: "App",
