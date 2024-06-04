44d430560ebeaba83b2017b5923a1d8901e9a660
Marcel W <st163937@stud.uni-stuttgart.de>
2024-06-04 12:22:26 +0200

    debug build

--- Prototype/captcha-frontend/src/main.js
+++ Prototype/captcha-frontend/src/main.js
@@ -1,7 +1,6 @@
-import { createApp, render } from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
-import router from './router'
-
+import router from "./router";
 
 
 createApp(App).use(router).mount("#app");
