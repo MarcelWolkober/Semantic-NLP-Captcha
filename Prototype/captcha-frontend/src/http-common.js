51bb4682ba419037272bfa91a6c52f6886c59421
Marcel W <st163937@stud.uni-stuttgart.de>
2024-06-05 16:33:42 +0200

    back end api corse change

--- Prototype/captcha-frontend/src/http-common.js
+++ Prototype/captcha-frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://captcha-back-end:8081/api",
+  baseURL: "http://back-end:8081/api",
   headers: {
     "Content-type": "application/json",
 
